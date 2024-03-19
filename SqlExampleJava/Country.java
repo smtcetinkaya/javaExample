acea5c2f08f4ac0a4e1b5d49ad600bcb835988bb
smtcetinkaya <132819307+smtcetinkaya@users.noreply.github.com>
2024-03-19 15:58:36 +0300

    Add files via upload

---
+++ SqlExampleJava/Country.java
@@ -0,0 +1,13 @@
+public class Country {
+    private String code;
+    private String name;
+    private String continent;
+    private String region;
+    public Country (String code,String name, String region, String continent)
+    {
+        this.code = code;
+        this.region = region;
+        this.continent = continent;
+        this.name = name;
+    }
+}
